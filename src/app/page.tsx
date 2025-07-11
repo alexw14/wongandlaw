8a89a9613939e8087b905e940307fedf64ef42e3
Alex Wong <alexw14@gmail.com>
2025-07-10 22:11:54 -0700

    add key values section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/HeroSection';
 import AboutStatsSection from '@/components/AboutStatsSection';
+import KeyValuesSection from '@/components/KeyValuesSection';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <AboutStatsSection />
+      <KeyValuesSection />
     </>
   );
 }
