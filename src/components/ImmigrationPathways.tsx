881bbd6a45dbfbecfbb4ecb43d35a5e6a4cd110b
Alex Wong <alexw14@gmail.com>
2025-07-14 18:29:09 -0700

    fix build error

--- src/components/ImmigrationPathways.tsx
+++ src/components/ImmigrationPathways.tsx
@@ -177,7 +177,7 @@ const ImmigrationPathways: React.FC<ImmigrationPathwaysProps> = ({
           whileInView="visible"
           viewport={{ once: true, amount: 0.2 }}
         >
-          {employmentBasedImmigration.items.map((item, idx) => (
+          {employmentBasedImmigration.items.map((item) => (
             <motion.div key={item.title} variants={sectionVariants}>
               <ImmigrationInfoCard
                 icon={getImmigrationInfoCardIcon(item.title)}
