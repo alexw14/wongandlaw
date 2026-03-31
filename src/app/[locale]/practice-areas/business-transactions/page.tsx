bf1a531c48ee77f4c95b5518cbf094e27bfbf0bc
Alex Wong <awong@ad.arrowheadproducts.net>
2026-03-31 16:26:58 -0700

    business transaction page set up

--- src/app/[locale]/practice-areas/business-transactions/page.tsx
+++ src/app/[locale]/practice-areas/business-transactions/page.tsx
@@ -1,9 +1,30 @@
-import ComingSoon from '@/components/ComingSoon';
+import React from 'react';
+import { useTranslations } from 'next-intl';
+import PracticeAreaHeader from '@/components/PracticeAreaHeader';
+import PracticeAreaOverview from '@/components/PracticeAreaOverview';
 
 const businessTransactionsPage = () => {
+  const t = useTranslations('BusinessTransactions');
+
+  const headerData = {
+    imgUrl: '/images/services/business-transactions.jpg',
+    imgAltText: 'business transactions',
+    title: t('title'),
+    subTitle: t('subTitle'),
+  };
+
+  const overviewData = {
+    overviewTitle: t('overviewTitle'),
+    overviewText: t('overviewText'),
+    aboutHeaderText: t('aboutHeaderText'),
+    aboutText: t('aboutText'),
+  };
+
   return (
-    <div>
-      <ComingSoon />
+    <div className="max-w-5xl mx-auto px-4 py-12">
+      <PracticeAreaHeader {...headerData} />
+      <PracticeAreaOverview {...overviewData} />
+
     </div>
   );
 };
